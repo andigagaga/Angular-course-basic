d666ba265101a5f50b7f604f609a09f45ec3bd69
andigagaga <andigagaga1@gmail.com>
2024-06-16 16:51:27 +0700

    menambahkan output ke event custom component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './user/dummy.users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'my-course';
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log('onSelectUser' + id);
+  }
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,40 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  computed,
+  signal,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  // get image nya
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
+}
