8a2219754a15fd93365287e14ee241c6dfc78a98
andigagaga <andigagaga1@gmail.com>
2024-06-16 19:22:50 +0700

    loopDataArray

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,6 +7,12 @@ import {
   signal,
 } from '@angular/core';
 
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -15,11 +21,7 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) user!: {
-    id: string;
-    name: string;
-    avatar: string;
-  };
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter();
 
   get imagePath() {
